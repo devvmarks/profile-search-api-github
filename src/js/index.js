9366e5e9716983453fab86af4b19940b660a1d3e
Davi Marques <Davichamico@gmail.com>
2023-05-08 14:55:38 -0300

    fazendo validaÇões e arrumando algumas coisas

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
-import { getUser } from "./services/user.js"
-import { getRepositories } from "./services/repositories.js"
-import { user } from "./objects/user.js"
+import { getUser } from "../js/services/user.js"
+import { getRepositories } from "../js/services/repositories.js"
+
+import { user } from "../js/objects/user.js"
 import { screen } from "../js/objects/screen.js"
 
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
+    if (validateEmptyInput(userName)) return
     getUserData(userName)
 })
 
@@ -15,13 +17,28 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const isEnterKeyPressed = key === 13
 
     if (isEnterKeyPressed) {
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
 
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
+
+
 async function getUserData(userName) {
 
     const userResponse = await getUser(userName)
+
+    if (userResponse.message === "Not Found") {
+        screen.renderNotFound()
+        return
+    }
+
     const repositoriesResponse = await getRepositories(userName)
 
     user.setInfo(userResponse)
--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -12,13 +12,15 @@ const screen = {
         let repositoriesItens = ''
         user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
 
-        if (user.repositories.lenght > 0) {
-            this.userProfile.innerHTML = + `<div class="repositories section">
+        if (user.repositories.length > 0) {
+            this.userProfile.innerHTML += `<div class="repositories section">
                                             <h2>Repositórios</h2>
                                             <ul>${repositoriesItens}</ul>
                                             </div>`
         }
-
+    },
+    renderNotFound(){
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3> "
     }
 }
 export { screen }
